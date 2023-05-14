a100b8679b5a122c26f3d0532729c3d81ffa74ce
Andrey Kravchenko <andreykravchenkodeveloper@gmail.com>
2023-05-14 19:45:52 +0300

    added missed exception

---
+++ src/main/java/com/andriikravchenkoo/carsaleproject/exception/ImageCompressException.java
@@ -0,0 +1,11 @@
+package com.andriikravchenkoo.carsaleproject.exception;
+
+public class ImageCompressException extends RuntimeException {
+
+    public ImageCompressException() {
+    }
+
+    public ImageCompressException(String message) {
+        super(message);
+    }
+}
